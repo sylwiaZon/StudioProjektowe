2b15b8bca5e57f6f5fb33135c7091ee5a2352977
Karolina Pieszczek <34057260+pyszczekk@users.noreply.github.com>
2020-04-02 18:57:01 +0200

    login View

--- spaceduck/src/Views/LoginView.jsx
+++ spaceduck/src/Views/LoginView.jsx
@@ -1,18 +1,49 @@
 import React from 'react';
 import Header from '../components/Header.jsx'
-
+import './forms-style.css'
+import nyanDuck from '../assets/Nyan_kaczka.png'
 class Login extends React.Component {
-    constructor(props) {
-        super(props);
-       
+    constructor() {
+        super();
+        this.state = {
+            mail: '',
+            password:''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handlePassword = this.handlePassword.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+           
     }
-   
+    handleChange(event) {
+        this.setState({mail: event.target.value});
+        console.log(event.target.value)
+    }
+    handlePassword(event) {
+        this.setState({password: event.target.value});
+        console.log(event.target.value)
+    }
+
+    handleSubmit(event) {
+        console.log('tutaj wolanie na backend')
+    }
     render() {
       
         return (
             <div className="app">
                 <Header />
-               <p>Login</p>
+                <h2 className="form-title">Logowanie</h2>
+                <img src={nyanDuck} alt="nyan kaczka" className="nyan" />
+                <div className="form-container">
+                    
+                    <form onSubmit={this.handleSubmit}>
+                    
+                      <input type="text" value={this.state.mail} onChange={this.handleChange} placeholder="email" defaultValue={this.state.mail} value={this.state.mail}/><br/>
+                       <input type="password" value={this.state.password} onChange={this.handlePassword} placeholder="password" defaultValue={this.state.password}/><br/>
+                   
+                    <input type="submit" value="Zaloguj" />
+                  </form>
+                </div>
 
             </div>
         )
