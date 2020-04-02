036a65ce0f24030a24cbb14f63eb52aed785eb26
Karolina Pieszczek <34057260+pyszczekk@users.noreply.github.com>
2020-04-02 15:09:50 +0200

    Main View + Router

---
+++ spaceduck/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Router } from 'react-router-dom';
+import Routes from './RouterApp.jsx';
+import history from './history.jsx';
+import './App.css';
+function App() {
+  return (
+    <Router history={history}>
+      <Routes />
+    </Router>
+  );
+}
+export default App;
---
+++ spaceduck/src/Views/IslandsView.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import Header from '../components/Header.jsx'
+import Kalambury from '../components/IslandKalambury.jsx'
+import Szachy from '../components/IslandSzachy.jsx'
+import Chinczyk from '../components/IslandChinczyk.jsx'
+import Statki from '../components/IslandStatki.jsx'
+import './island.css'
+import planet from '../assets/title-planet.png'
+import ufo from '../assets/title-ufo.png'
+
+
+class Islands extends React.Component {
+    constructor(props) {
+        super(props);
+        this.mouseOver = this.mouseOver.bind(this);
+        this.mouseOut = this.mouseOut.bind(this);
+        this.state = {
+            title: "Wybierz grę"
+        }
+       
+    }
+     mouseOver(name) {
+        this.setState({title: name});
+    }
+    mouseOut() {
+        this.setState({title: "Wybierz grę"});
+    }
+    
+    render() {
+       
+        return (
+            <div className="App" >
+                <Header />
+                <div className="Title"><img src={ufo} alt="ufo"/><p>{this.state.title}</p><img src={planet} alt="planet"/></div>
+                <div className="Islands" >
+                        <div onMouseOver={()=>this.mouseOver("Kalambury")} onMouseOut={this.mouseOut} >
+                           <a href="http://google.com" style={{display: 'inline-block'}}> <Kalambury /></a>
+                        </div>
+                        <div onMouseOver={()=>this.mouseOver("Szachy")} onMouseOut={this.mouseOut} >
+                             <a href="http://google.com" style={{display: 'inline-block'}}> <Szachy /></a>
+                        </div>
+                        <div onMouseOver={()=>this.mouseOver("Statki")} onMouseOut={this.mouseOut} >
+                            <a href="http://google.com" style={{display: 'inline-block'}}> <Statki /></a>
+                        </div>
+                        <div onMouseOver={()=>this.mouseOver("Chińczyk")} onMouseOut={this.mouseOut} >    
+                            <a href="http://google.com" style={{display: 'inline-block'}}> <Chinczyk /></a>
+                        </div>
+            
+                </div>
+
+            </div>
+        )
+    }
+}
+
+
+export default Islands;
