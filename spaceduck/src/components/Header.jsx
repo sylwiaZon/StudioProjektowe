a857f8d6d9b8fefc2eca8bca6fd40ce8b56321b1
Damian Kozyra <fizomik@gmail.com>
2020-04-05 15:12:28 +0200

    profileView

--- spaceduck/src/components/Header.jsx
+++ spaceduck/src/components/Header.jsx
@@ -19,6 +19,7 @@ class Header extends React.Component {
             <div className="buttons-container">
             {this.state.path==="/" || this.state.path==="/register"? <a href="/login" className="header-button"> Zaloguj </a>:null}
               {this.state.path==="/" || this.state.path==="/login"? <a href="/register" className="header-button"> Zarejestruj </a>:null}
+              {this.state.path==="/" || this.state.path==="/profile"? <a href="/profile" className="header-button"> Profil </a>:null}
 
             </div>
             </header>
