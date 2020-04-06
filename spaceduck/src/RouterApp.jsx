18f2b8ddefd8f7185992409946d2da2597e7115f
Damian Kozyra <fizomik@gmail.com>
2020-04-07 01:28:38 +0200

    Updated Profile Views

--- spaceduck/src/RouterApp.jsx
+++ spaceduck/src/RouterApp.jsx
@@ -4,6 +4,10 @@ import Islands from './Views/IslandsView.jsx';
 import Login from './Views/LoginView.jsx';
 import Register from './Views/RegisterView.jsx';
 import Profile from './Views/ProfileView.jsx';
+import Statistics from './Views/StatisticsView.jsx';
+import ChangePassword from './Views/ChangePasswordView.jsx';
+import DeleteAccount from './Views/DeleteAccountView.jsx';
+
 
 export default function Routes() {
   return (
@@ -11,7 +15,10 @@ export default function Routes() {
       <Route path="/" exact component={Islands} />
       <Route path="/register" component={Register} />
       <Route path="/login" component={Login}  />
-      <Route path="/profile" component={Profile}  />     
+      <Route path="/profile" component={Profile}  />
+      <Route path="/statistics" component={Statistics}  />     
+      <Route path="/changePassword" component={ChangePassword}  />     
+      <Route path="/deleteAccount" component={DeleteAccount}  />
       {/* redirect user to SignIn page if route does not exist and user is not authenticated */}
       <Route component={Islands} />
     </Switch>
