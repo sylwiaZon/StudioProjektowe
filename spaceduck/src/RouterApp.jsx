966bc9ace1a46486a199781c1bf357d77b00ef06
Karolina Pieszczek <34057260+pyszczekk@users.noreply.github.com>
2020-04-10 13:50:47 +0200

    login and register - connected to backend

--- spaceduck/src/RouterApp.jsx
+++ spaceduck/src/RouterApp.jsx
@@ -3,6 +3,10 @@ import { Switch, Route } from 'react-router-dom';
 import Islands from './Views/IslandsView.jsx';
 import Login from './Views/LoginView.jsx';
 import Register from './Views/RegisterView.jsx';
+import Profile from './Views/ProfileView.jsx';
+import Statistics from './Views/StatisticsView.jsx';
+import ChangePassword from './Views/ChangePasswordView.jsx';
+import DeleteAccount from './Views/DeleteAccountView.jsx';
 
 export default function Routes() {
   return (
@@ -10,6 +14,10 @@ export default function Routes() {
       <Route path="/" exact component={Islands} />
       <Route path="/register" component={Register} />
       <Route path="/login" component={Login}  />
+      <Route path="/profile" component={Profile}  />
+      <Route path="/statistics" component={Statistics}  />     
+      <Route path="/changePassword" component={ChangePassword}  />     
+      <Route path="/deleteAccount" component={DeleteAccount}  />
       
       {/* redirect user to SignIn page if route does not exist and user is not authenticated */}
       <Route component={Islands} />
--- spaceduck/src/Views/LoginView.jsx
+++ spaceduck/src/Views/LoginView.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import { useCookies } from 'react-cookie';
+import { Redirect } from 'react-router-dom'
 import Header from '../components/Header.jsx'
 import './forms-style.css'
 import nyanDuck from '../assets/Nyan_kaczka.png'
+import Cookies from 'universal-cookie';
+import address from '../configuration.json';
 
+const cookies = new Cookies();
 class Login extends React.Component {
+
     constructor() {
+        
         super();
         this.state = {
             name: '',
-            password:''
+            password:'',
+            id:123,
+            logged:false,
+            
         };
 
         this.handleChange = this.handleChange.bind(this);
@@ -22,17 +32,65 @@ class Login extends React.Component {
     }
     handlePassword(event) {
         this.setState({password: event.target.value});
-       
     }
 
     handleSubmit(event) {
-      
-       console.log('tutaj wolanie na backend')
+       
+       
+    //calling backend
+      fetch('https://'+address.backendURL+address.login, {
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json' 
+                    },
+                    body: JSON.stringify({
+                        "Name": this.state.name,
+                        "Password": this.state.password
+                    }),
+                })
+            .then((response) => {
+           
+                if(response.status==401){
+                //infromation about wrong login or password
+                 var error = document.createElement('p');
+                 error.style="color: #D62222; background: rgba(255,0,0,0.1); display:inline-block; padding:10px; width:80%; border: solid 1px #D62222;";
+                 error.innerHTML="Błędne hasło lub login/email!";
+                 var errorDiv = document.getElementsByClassName('errorContainer')[0];
+       
+                 if(errorDiv.childElementCount!=0)
+                    errorDiv.removeChild(errorDiv.lastElementChild);
+                 errorDiv.style="visibility:visible;";
+                 errorDiv.append(error);
+                 
+                }
+                if(response.status==200){
+                    //success -> login 
+                    response.json()
+                    .then((responseJson) => {
+                   cookies.set('user', responseJson, { path: '/' });
+                   
+                    this.setState({logged: true});
+                
+                
+              })
+         }
+     })
+              
+            .catch((error) => {
+                 console.error(error)
+              
+        });
+
+
      
     }
 
     render() {
-      
+        //redirect to main view
+       if (this.state.logged === true) {
+          return <Redirect to='/' />
+        }
         return (
             <div className="app">
                 <Header />
@@ -40,13 +98,13 @@ class Login extends React.Component {
                 <img src={nyanDuck} alt="nyan kaczka" className="nyan" />
                 <div className="form-container">
                     
-                    <form onSubmit={this.handleSubmit} action="/">
-                    
+                    <div>
+                     <div className='errorContainer'></div>
                       <input type="text" value={this.state.name} onChange={this.handleChange} placeholder="login/email" defaultValue={this.state.mail} /><br/>
-                       <input type="password" value={this.state.password} onChange={this.handlePassword} placeholder="hasło" defaultValue={this.state.password}/><br/>
+                       <input type="password"  onChange={this.handlePassword} placeholder="hasło" defaultValue={this.state.password}/><br/>
                    
-                    <input type="submit" value="Zaloguj" />
-                  </form>
+                    <input type="submit" value="Zaloguj" onClick={this.handleSubmit} />
+                  </div>
                 </div>
 
             </div>
