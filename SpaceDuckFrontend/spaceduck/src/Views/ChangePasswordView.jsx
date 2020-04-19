b7a1512625d1c296b4e282d8bc1b040e0973b496
sylwiaZon <sylwia.zon@gmail.com>
2020-04-19 12:28:00 +0200

    Revert "initial commit"
    This reverts commit 8fe49d0ec11c20af79e37e45f7c43bd34851fa14.

--- SpaceDuckFrontend/spaceduck/src/Views/ChangePasswordView.jsx
+++ SpaceDuckFrontend/spaceduck/src/Views/ChangePasswordView.jsx
@@ -2,22 +2,13 @@ import React from 'react';
 import Header from '../components/Header.jsx'
 import './forms-style.css'
 import nyanDuck from '../assets/Nyan_kaczka.png'
-import address from '../configuration.json';
 
 class ChangePassword extends React.Component {
     constructor() {
         super();
         this.state = {
-            id:'',
-            name:'',
-            email:'',
-            password:'',
             oldPassword: '',
-            newPassword:'',
-            correctOldPassword:false,
-            correctNewPassword:false,
-            correctData:false,
-            changed:false
+            newPassword:''
         };
 
         this.handleOldPassword = this.handleOldPassword.bind(this);
@@ -37,110 +28,24 @@ class ChangePassword extends React.Component {
         }
     
         handleSubmit(event) {
-    
-            fetch('https://'+address.backendURL+address.userPath+this.state.id, {
-                        method: 'POST',
-                        headers: {
-                            'Accept': 'application/json',
-                            'Content-Type': 'application/json' 
-                        },
-                        body: JSON.stringify({
-                            "Name": this.state.name,
-                            "Email": this.state.email,
-                            "Password": this.state.oldPassword,
-                            "NewPassword": this.state.newPassword
-                        }),
-                    })
-                .then((response) => response.json())
-                .then((responseJson) => {
-                    if(responseJson=="You can't change password!"){
-                    //information about taken login/email
-                     var error = document.createElement('p');
-                     error.style="color: #D62222; background: rgba(255,0,0,0.1); display:inline-block; padding:10px; width:80%; border: solid 1px #D62222;";
-                     error.innerHTML="Błędne hasło!";
-                     var errorDiv = document.getElementsByClassName('errorContainer')[0];
-                     if(errorDiv.childElementCount!=0)
-                        errorDiv.removeChild(errorDiv.lastElementChild);
-                     errorDiv.style="visibility:visible;";
-                     errorDiv.append(error);
-                     
-                    }else{
-                        //changeded password succesfully 
-                        this.setState({changed: true});
-                    }
-                    
-                  })
-                  
-                .catch((error) => {
-                     var error = document.createElement('p');
-                     error.style="color: #D62222; background: rgba(255,0,0,0.1); display:inline-block; padding:10px; width:80%; border: solid 1px #D62222;";
-                     error.innerHTML="Nie można połączyć się z serwerem, spróbuj poźniej!";
-                     var errorDiv = document.getElementsByClassName('errorContainer')[0];
-           
-                     if(errorDiv.childElementCount!=0)
-                        errorDiv.removeChild(errorDiv.lastElementChild);
-                     errorDiv.style="visibility:visible;";
-                     errorDiv.append(error);
-                  
-            });
-        }
-
-        strongPassword(newPassword){
-            var re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
-            return re.test(newPassword)
+           console.log('tutaj wolanie na backend')
         }
 
     render() {
-        
-        let inputStyle = { };
-        if(this.state.password!==this.state.oldPassword){
-            inputStyle={
-                'border': 'solid red 1px',
-                'background': 'rgba(255,0,0,0.3)'
-            }
-             this.state.correctOldPassword = false;
-
-        }else{
-            inputStyle={}   
-            this.state.correctOldPassword = true; 
-        }
-        let passwordStyle = {};
-        //strong password validation
-        if(!this.strongPassword(this.state.newPassword)){
-            passwordStyle = {
-                'border': 'solid red 1px',
-                'background': 'rgba(255,0,0,0.3)'
-            }
-             this.state.correctNewPassword = false;
-        }else{
-            passwordStyle={};
-            this.state.correctNewPassword = true;
-        }
-
-        if(this.state.correctOldPassword && this.state.correctNewPassword){
-            this.state.correctData=true;
-        }else{
-            this.state.correctData=false;
-        }
-        
+      
         return (
             <div className="app">
                 <Header />
                 <h2 className="form-title">zmień hasło</h2>
                 <img src={nyanDuck} alt="nyan kaczka" className="nyan" />
-                
                 <div className="form-container">
-                    <div className='errorContainer'></div>
-
-                    <div>
-                        <h2 className="form-white-title">podaj stare oraz nowe hasło i potwierdź</h2>
-                        <input style={passwordStyle} type="password" value={this.state.oldPassword} onChange={this.handleOldPassword} placeholder="stare hasło" defaultValue={this.state.oldPassword}/><br/>
-                        {!this.state.correctNewPassword ? <p className='incorrectPassword'>Użyj silnego hasła. Silne hasło zawiera minimum jedną dużą i małą literę, cyfrę oraz znak specialny. Minimalna długość hasła to 8 znaków.</p>:null}
-                        <input style={passwordStyle} type="password" value={this.state.newPassword} onChange={this.handleNewPassword} placeholder="nowe hasło" defaultValue={this.state.newPassword}/><br/>
-                        {this.state.correctNewPassword ? (<input type="submit" style={{minWidth:230}} value="zmień" onClick={this.handleSubmit} enabled="true"/>) : (<input type="submit" value="Zmień" style={{minWidth:230}} disabled="true"/>)}
-                        <a href="/profile" className="return-button"> powrót </a>
-                    </div>
-                
+                <form onSubmit={this.handleSubmit} action="/">
+                    <h2 className="form-white-title">podaj stare oraz nowe hasło i potwierdź</h2>
+                    <input type="password" value={this.state.oldPassword} onChange={this.handleOldPassword} placeholder="stare hasło" defaultValue={this.state.oldPassword}/><br/>
+                    <input type="password" value={this.state.newPassword} onChange={this.handleNewPassword} placeholder="nowe hasło" defaultValue={this.state.newPassword}/><br/>
+                    <input type="submit" style={{minWidth:230}} value="zmień"/> 
+                    <a href="/profile" className="return-button"> powrót </a>
+                </form>
                 </div>
             </div>
         )
