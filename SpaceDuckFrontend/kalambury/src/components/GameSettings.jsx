408de70338a622de218235d7c71d67a259431863
Karolina Pieszczek <34057260+pyszczekk@users.noreply.github.com>
2020-04-20 19:45:48 +0200

    keeping guest name, settings table validation

--- SpaceDuckFrontend/kalambury/src/components/GameSettings.jsx
+++ SpaceDuckFrontend/kalambury/src/components/GameSettings.jsx
@@ -17,10 +17,66 @@ class GameSettings extends React.Component{
   };
 	constructor(...props){
 		super(...props);
+		this.state = {
+			roundNumber: 1,
+			roundMinute:1,
+			roundSeconds:0,
+			correctData: true,
+		}
+		
+		this.handleRoundNumber = this.handleRoundNumber.bind(this);
+		this.handleRoundMinutes = this.handleRoundMinutes.bind(this);
+		this.handleRoundSeconds = this.handleRoundSeconds.bind(this);
+		this.handleNumbersOnly = this.handleNumbersOnly.bind(this);
+		this.handleNumbersOnly2 = this.handleNumbersOnly2.bind(this);
+		this.handleNumbersOnly3 = this.handleNumbersOnly3.bind(this);
 	}
-	
-	
+	handleNumbersOnly(event){
+		if(event.keyCode<48 || event.keyCode>57){
+			this.setState({roundNumber: ''});
+
+		}
+	}
+	handleNumbersOnly2(event){
+		if(event.keyCode<48 || event.keyCode>57){
+			this.setState({roundMinute: ''});
+
+		}
+	}
+	handleNumbersOnly3(event){
+		if(event.keyCode<48 || event.keyCode>57){
+			this.setState({roundSeconds: ''});
+
+		}
+	}
+	handleRoundNumber(event){
+		this.setState({roundNumber: event.target.value})
+	}
+	handleRoundMinutes(event){
+		this.setState({roundMinute: event.target.value})
+	}
+	handleRoundSeconds(event){
+		this.setState({roundSeconds: event.target.value})
+	}
 	render(){
+
+		if(this.state.roundNumber==0){
+			this.state.correctData=false;
+		}else{
+			if(this.state.roundMinute == 0){
+				if(this.state.roundSeconds>=30 && this.state.roundSeconds<60){
+					this.state.correctData=true;
+				}else{
+					this.state.correctData=false;
+				}
+			}else{
+				if(this.state.roundSeconds>=0 && this.state.roundSeconds<60){
+					this.state.correctData=true;
+				}else{
+					this.state.correctData=false;
+				}
+			}
+		}
 		const {
 			privateTable,
 		 	handlePrivateTable,
@@ -36,11 +92,12 @@ class GameSettings extends React.Component{
 				</div>
 				<div className="settingsTile">
 				<div>
-				<label>ilość tur <span><input type="number" className="settingsInput"/></span></label>
-				<p>czas trwana tury <span><input type="number" className="timeInput"/> : <input type="number"className="timeInput" /></span></p>
+				<label>ilość tur <span><input type="text" className="settingsInput" onKeyUp={this.handleNumbersOnly} onChange={this.handleRoundNumber} value={this.state.roundNumber}/></span></label>
+				<p>czas trwana tury <span><input type="text" className="timeInput" onKeyUp={this.handleNumbersOnly2} value={this.state.roundMinute} onChange={this.handleRoundMinutes}/> : <input type="text"className="timeInput" onKeyUp={this.handleNumbersOnly3} value={this.state.roundSeconds}  onChange={this.handleRoundSeconds}/></span></p>
 				</div>
 				</div>
-				<button onClick={this.props.continueFunc}>kontynuuj</button>
+				{this.state.correctData ? <button onClick={this.props.continueFunc}>kontynuuj</button>: <div><p className="error settingsTile">Uzupełnij prawidłowo formularz</p> <button disabled>Kontunuuj</button></div>}
+				
 			</div>
 			)
 	}
