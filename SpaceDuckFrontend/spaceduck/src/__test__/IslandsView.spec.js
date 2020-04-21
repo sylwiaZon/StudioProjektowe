56d9d067dc5fbe46d3be2fb5d6e9670510477b60
Karolina Pieszczek <34057260+pyszczekk@users.noreply.github.com>
2020-04-21 21:30:07 +0200

    more tests

--- SpaceDuckFrontend/spaceduck/src/__test__/IslandsView.spec.js
+++ SpaceDuckFrontend/spaceduck/src/__test__/IslandsView.spec.js
@@ -5,33 +5,36 @@ import Islands from "../Views/IslandsView.jsx";
 describe("Islands View", () => {
 	const component = TestRenderer.create(<Islands />).root;    
     const instance = component.instance;
-
-test("setting  proper title", () => {
-    expect(instance.state.title).toBe("Wybierz grę");
     const container = component.findByProps({className: "islands"});
     const islands = container.findAllByType("a");
-    expect(islands.length).toBe(4)
-    for(let i = 0; i<islands.length ;i++){
-    
-    	islands[i].parent.props.onMouseOver();
-    	
-    	switch(i){
-    		case 0:
-    			expect(instance.state.title).toBe("Kalambury");
-    			break
-    		case 1:
-    			expect(instance.state.title).toBe("Szachy");
-    			break
-    		case 2:
-    			expect(instance.state.title).toBe("Statki");
-    			break
-    		case 3:
-    			expect(instance.state.title).toBe("Chińczyk");
-    			break
-    	}
-    	 islands[i].parent.props.onMouseOut();
+	test("init test", ()=>{
+		 expect(instance.state.title).toBe("Wybierz grę");
+	})
+	test("setting  proper title - kalambury", () => {
+	   	let i = 0;
+	    islands[i].parent.props.onMouseOver();
+	   expect(instance.state.title).toBe("Kalambury");
+	});
+	test("setting  proper title - szachy", () => {
+	   	let i = 1;
+	    islands[i].parent.props.onMouseOver();
+	   expect(instance.state.title).toBe("Szachy");
+	});
+	test("setting  proper title - statki", () => {
+	   	let i = 2;
+	    islands[i].parent.props.onMouseOver();
+	   expect(instance.state.title).toBe("Statki");
+	});
+    test("setting  proper title - chinczyk", () => {
+	   	let i = 3;
+	    islands[i].parent.props.onMouseOver();
+	   expect(instance.state.title).toBe("Chińczyk");
+	});	
+    		
+    test("mouse out of islands", ()=>{
+    	 islands[2].parent.props.onMouseOut();
          expect(instance.state.title).toBe("Wybierz grę");
-    }
-  
-  });
+    })	
+
+
 });
--- SpaceDuckFrontend/spaceduck/src/__test__/LoginView.spec.js
+++ SpaceDuckFrontend/spaceduck/src/__test__/LoginView.spec.js
@@ -6,11 +6,12 @@ describe("Login View", () => {
 	const component = TestRenderer.create(<LoginView />).root;    
     const instance = component.instance;
 
-test("testing login inputs", () => {
+test("testing login/email", () => {
 	const login = component.findByProps({placeholder: "login/email"})
 	login.props.onChange({target: {value: "test"}});
 	expect(instance.state.name).toBe(login.props.value);
-
+})
+test("testing password", () => {
 	const password = component.findByProps({placeholder: "hasło"})
 	password.props.onChange({target: {value: "test"}});
 	expect(instance.state.password).toBe(password.props.value);
