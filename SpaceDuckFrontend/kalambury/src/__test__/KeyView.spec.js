546b80f85610b38760b2f8dca46bae1201b248b6
sylwiaZon <sylwia.zon@gmail.com>
2020-04-21 21:15:48 +0200

    fix kalambury tests

--- SpaceDuckFrontend/kalambury/src/__test__/KeyView.spec.js
+++ SpaceDuckFrontend/kalambury/src/__test__/KeyView.spec.js
@@ -6,10 +6,9 @@ describe("Game Key View", () => {
 	const component = TestRenderer.create(<KeyView />).root;    
     const instance = component.instance;
 
-test("check proper key", () => {
-   
-  	const key = component.findByProps({className:"settings-container"})
-  	const keyValue = key.findAllByType("h3")[0];
-  	expect(component.props.keyValue).toBe(keyValue.props.children)
+	test("check key", () => {
+		const key = component.findByProps({className:"settings-container"})
+		const keyValue = key.findAllByType("h3")[0];
+		expect(component.props.keyValue).toBe(keyValue.props.children)
   });
 });
