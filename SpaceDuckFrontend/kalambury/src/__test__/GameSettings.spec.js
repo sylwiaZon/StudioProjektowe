8b13e43b1ac500cadd5767594d4cf12047d9d416
Karolina Pieszczek <34057260+pyszczekk@users.noreply.github.com>
2020-04-21 20:59:17 +0200

    more tests

--- SpaceDuckFrontend/kalambury/src/__test__/GameSettings.spec.js
+++ SpaceDuckFrontend/kalambury/src/__test__/GameSettings.spec.js
@@ -5,57 +5,65 @@ import GameSettings from "../components/GameSettings.jsx";
 describe("Game Settings", () => {
 	const component = TestRenderer.create(<GameSettings />).root;    
     const instance = component.instance;
-
- test("testing round number ", () => {
+describe("Round Number ",() => {
+	test("round number 0 ", () => {
+	   
+	   instance.setState({roundNumber:0})
+	   expect(instance.state.correctData).toBe(false)
+	    
+	});
+	test("round number not 0 ", () => {
    
-   instance.setState({roundNumber:0})
-   expect(instance.state.correctData).toBe(false)
-    instance.setState({roundNumber:5})
-    expect(instance.state.correctData).toBe(true)
-});
- test("testing round time ",() => {
-    instance.setState({roundMinute:0})
-    for (let i = 0; i < 30; i += 1){
-    	instance.setState({roundSeconds: i})
+	   
+	    instance.setState({roundNumber:5})
+	    expect(instance.state.correctData).toBe(true)
+	});
+})
+ describe("Round Time ",() => {
+	test("testing round time, 0minutes and less than 30seconds ",() => {
+    	instance.setState({roundMinute:0, roundSeconds:29})
     	expect(instance.state.correctData).toBe(false)
-    }
-    for (let i = 30; i < 60; i += 1){
-    	instance.setState({roundSeconds: i})
+    })
+    test("testing round time, more than 59seconds ",() => {
+    	instance.setState({roundSeconds:60})
+    	expect(instance.state.correctData).toBe(false)
+    })
+    test("testing round time,0minutes correct number of seconds ",() => {
+    	instance.setState({roundMinute:0,roundSeconds:30})
     	expect(instance.state.correctData).toBe(true)
-    }
-    for (let i = 60; i < 120; i += 1){
-    	instance.setState({roundSeconds: i})
-    	expect(instance.state.correctData).toBe(false)
-    }
-    instance.setState({roundMinute:12})
-    for (let i = 0; i < 60; i += 1){
-    	instance.setState({roundSeconds: i})
+    })
+    test("testing round time,more than minute ",() => {
+    	instance.setState({roundMinute:1,roundSeconds:1})
     	expect(instance.state.correctData).toBe(true)
-    }
-   	for (let i = 60; i < 120; i += 1){
-    	instance.setState({roundSeconds: i})
-    	expect(instance.state.correctData).toBe(false)
-    }
-     instance.setState({roundMinute:"a12"})
-     expect(instance.state.correctData).toBe(false)
+    })
+   
+})
+ 
+describe("Round Time ",() => {
+	const rounds = component.findByProps({className: "settingsInput"})
+	const minute = component.findAllByProps({className: "timeInput"})[0]
+	 const second = component.findAllByProps({className: "timeInput"})[1]
+	 test("testing only digits inputs - round number", () => {
+	  
+	   rounds.props.onChange({target:{value:'lalala'}});
+	   rounds.props.onKeyUp({keyCode: 'lalala'.charCodeAt(0)})
+	   expect(instance.state.roundNumber).toBe('');
+	})
+	 test("testing only digits inputs - minutes", () => {
+	  
+	  minute.props.onChange({target:{value:'lalala'}});
+	   minute.props.onKeyUp({keyCode: 'lalala'.charCodeAt(0)})
+	   expect(instance.state.roundMinute).toBe('');
+	})
+	 test("testing only digits inputs - seconds", () => {
+	  
+	  second.props.onChange({target:{value:'lalala'}});
+	   second.props.onKeyUp({keyCode: 'lalala'.charCodeAt(0)})
+	  
+	   expect(instance.state.roundSeconds).toBe('');
+	})
 
-  });
+	   
 
- test("testing only digits inputs", () => {
-   const rounds = component.findByProps({className: "settingsInput"})
-
-   rounds.props.onChange({target:{value:'lalala'}});
-   rounds.props.onKeyUp({keyCode: 'lalala'.charCodeAt(0)})
-   expect(instance.state.roundNumber).toBe('');
-
-   const minute = component.findAllByProps({className: "timeInput"})[0]
-   const second = component.findAllByProps({className: "timeInput"})[1]
-   minute.props.onChange({target:{value:'lalala'}});
-   minute.props.onKeyUp({keyCode: 'lalala'.charCodeAt(0)})
-   second.props.onChange({target:{value:'lalala'}});
-   second.props.onKeyUp({keyCode: 'lalala'.charCodeAt(0)})
-   expect(instance.state.roundMinute).toBe('');
-   expect(instance.state.roundSeconds).toBe('');
-    expect(instance.state.correctData).toBe(false)
 });
 });
--- SpaceDuckFrontend/kalambury/src/__test__/Kalambury.spec.js
+++ SpaceDuckFrontend/kalambury/src/__test__/Kalambury.spec.js
@@ -3,24 +3,20 @@ import TestRenderer from "react-test-renderer";
 import Kalambury from "../views/Kalambury.jsx";
 
 describe("Main View", () => {
-  test("testing instruction popup", () => {
-    const component = TestRenderer.create(<Kalambury />).root;
-    
-   const instance = component.instance;
+	 const component = TestRenderer.create(<Kalambury />).root;
+	 const instance = component.instance;
+  test("testing init", () => {
+  
     expect(instance.state.instructionPopup).toBe(false);
-    
-  instance.setState({instructionPopup:true})
+    });
+   test("testing instruction popup", () => {
+	  instance.setState({instructionPopup:true})
 
-expect(component.findByProps({className: "instructionPopup"}))
-
+	expect(component.findByProps({className: "instructionPopup"}))
   });
   test("testing guest panel", () => {
-    const component = TestRenderer.create(<Kalambury />).root;
-    
-   const instance = component.instance;
   
- 	 expect(instance.state.guest).toBe(false);
-   instance.setState({guest:true})
+   	instance.setState({guest:true})
      expect(component.findByProps({className: "asGuest"}))
   });
 });
