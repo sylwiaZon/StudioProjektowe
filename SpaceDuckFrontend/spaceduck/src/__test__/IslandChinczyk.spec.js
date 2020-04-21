19197beeb8d06bb841cf079393ecbd24c1b503ee
sylwiaZon <sylwia.zon@gmail.com>
2020-04-21 22:16:13 +0200

    Fix Register tests

--- SpaceDuckFrontend/spaceduck/src/__test__/IslandChinczyk.spec.js
+++ SpaceDuckFrontend/spaceduck/src/__test__/IslandChinczyk.spec.js
@@ -7,17 +7,20 @@ describe("Island Chinczyk", () => {
     const instance = component.instance;
     const img = component.findByProps({className: "grid-box"})
     
-    test("testing hover init", () => {
-        expect(instance.state.hover).toBe(false);
+    describe("hover", ()=>{
+        test("init", () => {
+            expect(instance.state.hover).toBe(false);
+        });
+        
+        test("mouse over", ()=>{
+             img.props.onMouseOver();
+             expect(instance.state.hover).toBe(true);
+        });
+    
+        test("mouse out", () => {
+            img.props.onMouseOut();
+             expect(instance.state.hover).toBe(false);
+        });
     });
     
-    test("testing hover, mouse over", ()=>{
-         img.props.onMouseOver();
-         expect(instance.state.hover).toBe(true);
-    });
-
-    test("testing hover, mouse out", () => {
-        img.props.onMouseOut();
-         expect(instance.state.hover).toBe(false);
-    });
 });
