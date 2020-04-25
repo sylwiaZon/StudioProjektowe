7d51049564bf3e69f6c7e5e5f29ec18fa6575ecb
sylwiaZon <sylwia.zon@gmail.com>
2020-04-25 16:48:40 +0200

    Connect tables view with backend

--- SpaceDuckFrontend/kalambury/src/views/TablesView.jsx
+++ SpaceDuckFrontend/kalambury/src/views/TablesView.jsx
@@ -3,14 +3,28 @@ import Table from '../components/Table'
 import './tables-styles.css'
 import Header from '../components/Header'
 import addTable from '../assets/Guzik_Nowy_Stół.png';
-
+import address from '../configuration.json';
+import history from '../history.jsx';
 
 class Tables extends React.Component {
     constructor() {
         super();
-                 
+        this.state = {
+            tables: []
+        };
     }
 
+    componentDidMount(){
+        fetch('https://'+address.kalamburyURL+address.room+'/all')
+            .then((response) => response.json())
+            .then(data => {
+                this.setState({tables: data});
+                console.log(data);
+            })
+            .catch((error) => {
+                
+            });
+    }
 
     render() {
       
@@ -28,18 +42,13 @@ class Tables extends React.Component {
                         <li className="game-button">BUTTON</li>
                     </ul>
                     <ul>
-                        <li><Table/></li>
-                        <li><Table/></li>
-                        <li><Table/></li>
-                        <li><Table/></li>
-                        <li><Table/></li>
-                        <li><Table/></li>
-                        <li><Table/></li>
-                        <li><Table/></li>
-                        <li><Table/></li>
-                        <li><Table/></li>
+                        {this.state.tables.map(arg => 
+                            <li key={arg.id}>
+                                <Table id={arg.id} playersIds={['a0e38bfa-0df4-4374-9fbb-57e68902d8c7']} isFull={arg.isFull} roomConfiguration={arg.roomConfiguration}/>
+                            </li>
+                        )}
                     </ul>
-                    <button className="add-table table-button" type="button">
+                    <button className="add-table table-button" type="button" onClick={() => {history.push('/game')}}>
                         <img src={addTable} className="add-table-image" alt="Add table"/>
                     </button>
                 </div>
