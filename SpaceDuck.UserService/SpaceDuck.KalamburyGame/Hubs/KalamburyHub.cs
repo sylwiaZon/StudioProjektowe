7a2d3a87540d3bb1f20ee92d78b3e4fc5bafb837
SzymonBorowy <34072574+SzymonBorowy@users.noreply.github.com>
2020-05-06 21:12:30 +0200

    Add kalambury backend

---
+++ SpaceDuck.UserService/SpaceDuck.KalamburyGame/Hubs/KalamburyHub.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SpaceDuck.Common.Models;
+using SpaceDuck.KalamburyGame.Server;
+using SpaceDuck.KalamburyGame.Services;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SpaceDuck.KalamburyGame.Hubs
+{
+    public interface IKalamburyHub
+    {
+        Task SendMessage(string user, string message);
+        Task AddToGameGroup(string gameId, string playerName);
+        Task RemoveFromGameGroup(string gameId, string playerName);
+        Task SendGameStatus(string gameId, GameStatus gameStatus);
+        Task RecieveGameStatus(string gameId, GameStatus gameStatus);
+        Task SendMesage(string gameId, string message);
+        Task CheckGivenWord(string gameId, WordStatus wordStatus);
+        Task SendPoints(string gameId, Dictionary<string, int> points);
+    }
+
+    public class KalamburyHub : Hub, IKalamburyHub
+    {
+        private readonly IHubContext<KalamburyHub> _hubContext;
+        private IGameHelper _gameHelper;
+        private IRoomService _roomService;
+
+        public KalamburyHub(IHubContext<KalamburyHub> hubContext,
+            IGameHelper gameHelper,
+            IRoomService roomService)
+        {
+            _hubContext = hubContext;
+            _gameHelper = gameHelper;
+            _roomService = roomService;
+        }
+
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task AddToGameGroup(string gameId, string playerName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, gameId);
+
+            await SendToGameGroup(gameId, "Send", $"{playerName} has joined the game.");
+
+            await _roomService.AddPlayerToRoom(Convert.ToInt32(gameId), playerName);
+        }
+
+        public async Task RemoveFromGameGroup(string gameId, string playerName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, gameId);
+
+            await SendToGameGroup(gameId, "Send", $"{playerName} has left the game.");
+
+            await _roomService.RemovePlayerFromRoom(Convert.ToInt32(gameId), playerName);
+
+        }
+
+        public async Task SendGameStatus(string gameId, GameStatus gameStatus)
+        {
+            await _hubContext.Clients.Group(gameId).SendAsync("GameStatus", gameStatus);
+            await _hubContext.Clients.Group(gameId).SendAsync("Send", "Update status by contexthub.");
+        }
+
+        public async Task SendMesage(string gameId, string message)
+        {
+            await _hubContext.Clients.Group(gameId).SendAsync("Send", message);
+        }
+
+        public async Task SendPoints(string gameId, Dictionary<string, int> points)
+        {
+            await _hubContext.Clients.Group(gameId).SendAsync("Points", points);
+        }
+
+        public async Task RecieveGameStatus(string gameId, GameStatus gameStatus)
+        {
+            await SendToGameGroup(gameId, "GameStatus", gameStatus);
+        }
+
+        public async Task CheckGivenWord(string gameId, WordStatus wordStatus)
+        {
+            var isCorrect = _gameHelper.UpdateWordStatus(gameId, wordStatus);
+
+            if (isCorrect)
+                await SendMesage(gameId, $"Gracz {wordStatus.PlayerId} zgadł hasło {wordStatus.Word}");
+        }
+
+        private async Task SendToGameGroup(string gameId, string method, object arg)
+        {
+            await Clients.Group(gameId).SendAsync(method, arg);
+        }
+    }
+}
---
+++ SpaceDuck.UserService/SpaceDuck.KalamburyGame/Server/GameServer.cs
@@ -0,0 +1,117 @@
+﻿using SpaceDuck.Common.Models;
+using SpaceDuck.KalamburyGame.Services;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SpaceDuck.KalamburyGame.Server
+{
+    public interface IGameServer
+    {
+        public IGameHelper gameHelper { get; }
+        Task CreateGame(int roomId);
+        Task CreateGame(GameTask gameTask);
+        void UpdateGameStatus(string gameId, GameStatus gameStatus);
+    }
+
+    public class GameServer : IGameServer
+    {
+        private IKalaburyService kalaburyService;
+        private IGameMiddleware gameMiddleware;
+        public IGameHelper gameHelper { get; }
+
+        public GameServer(IKalaburyService kalaburyService,
+            IGameMiddleware gameMiddleware,
+            IGameHelper gameHelper)
+        {
+            this.kalaburyService = kalaburyService;
+
+            this.gameMiddleware = gameMiddleware;
+
+            this.gameHelper = gameHelper;
+
+            Thread thread = new Thread(StartServer);
+            thread.Start();
+        }
+
+        private void StartServer()
+        {
+            Func<Task<string>> generateWordMethod = kalaburyService.GetWord;
+            Func<Game, string> generateCurrentPlayerMethod = kalaburyService.SelectCurrentPlayer;
+            Console.WriteLine("Server started.");
+
+            while (true)
+            {
+                Console.WriteLine($"Game count: {gameHelper.gameTasks.Count}");
+                Console.WriteLine($"Running game count: {gameHelper.gameTasks.Where(g => g.IsStarted).ToList().Count}");
+
+                foreach (var gameTask in gameHelper.gameTasks)
+                {
+                    if (!gameTask.IsStarted)
+                        continue;
+
+                    gameTask.CheckRound();
+
+                    gameTask.CheckStatus();
+
+                    if (gameTask.IsFinshed)
+                    {
+                        if (!gameTask.WasGuessed)
+                            gameTask.UpdatePoints(null);
+                        gameMiddleware.SendPoints(gameTask.Game.Room.Id.ToString(), gameTask.Game.PlayersPointsPerGame);
+                        gameMiddleware.SendMesage(gameTask.Game.Room.Id.ToString(), $"Koniec rundy, hasło: {gameTask.GameStatus.Word}");
+                        gameTask.GenerateNewRound(generateWordMethod, generateCurrentPlayerMethod);
+
+                        continue;
+                    }
+
+                    if (gameTask.IsEnded)
+                    {
+                        kalaburyService.UpdateUsersPoints(gameTask.Game.PlayersPointsPerGame);
+                        gameTask.GameStatus.IsFinished = true;
+                        gameMiddleware.SendMesage(gameTask.Game.Room.Id.ToString(), "Koniec gry");
+                        continue;
+                    }
+
+                    gameMiddleware.SendGameStatus(gameTask.Game.Room.Id.ToString(), gameTask.GameStatus);
+                }
+
+                gameHelper.gameTasks.RemoveAll(game => game.IsEnded);
+
+
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("Server ended.");
+        }
+
+        public async Task CreateGame(int roomId)
+        {
+            Func<Task<string>> generateWordMethod = kalaburyService.GetWord;
+            Func<Game, string> generateCurrentPlayerMethod = kalaburyService.SelectCurrentPlayer;
+
+            var gameTask = gameHelper.gameTasks.FirstOrDefault(game => game.Game.Room.Id == roomId);
+
+            await gameTask.GenerateNewRound(generateWordMethod, generateCurrentPlayerMethod);
+
+            gameMiddleware.SendGameStatus(gameTask.Game.Room.Id.ToString(), gameTask.GameStatus);
+            gameMiddleware.SendPoints(gameTask.Game.Room.Id.ToString(), gameTask.Game.PlayersPointsPerGame);
+
+            gameTask.IsStarted = true;
+        }
+
+        public void UpdateGameStatus(string gameId, GameStatus gameStatus)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task CreateGame(GameTask gameTask)
+        {
+            gameHelper.gameTasks.Add(gameTask);
+
+            CreateGame(gameTask.Game.Room.Id);
+        }
+    }
+
+
+}
