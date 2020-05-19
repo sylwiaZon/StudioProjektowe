1a42809886b99df3002752d6dbdc5689afae9b0f
ania wojcik <aniii.wojcik@gmail.com>
2020-05-19 17:50:31 +0200

    Update

--- SpaceDuck.UserService/SpaceDuck.Common/Models/Game.cs
+++ SpaceDuck.UserService/SpaceDuck.Common/Models/Game.cs
@@ -8,6 +8,13 @@ namespace SpaceDuck.Common.Models
     {
         public string Id { get; set; }
         public Room Room { get; set; }
+        public Dictionary<string, int> PlayersPointsPerGame { get; set; }
+    }
+
+    public class KalamburyGame : Game
+    {
+        public string CurrentPlayerId { get; set; }
+        public List<string> SubmittedForDrawing { get; set; } = new List<string>();
     }
 
     public class ChessGame : Game
@@ -17,11 +24,29 @@ namespace SpaceDuck.Common.Models
 
     public class GameStatus
     {
-        public string Word { get; set; }
+        public string Word { get; set; } = "";
+        public string Canvas { get; set; }
         public string CurrentPlayerId { get; set; }
-        public bool Finished { get; set; }
-        public bool Draw { get; set; }
-        public bool Resigned { get; set; }
+        public bool IsFinished { get; set; }
+        public string Hint { get; set; } = "";
 
+    }
+
+    public class ChessGameStatus
+    {
+        public string Board { get; set; } = "";
+        public string CurrentPlayerId { get; set; }
+        public bool IsFinished { get; set; }
+        public bool Resigned{ get; set; }
+        public bool DrawOffered { get; set; }
+        public bool DrawAccepted { get; set; }
+        public int Round { get; set; } = 0;
+        public int RoundTime { get; set; } = 0;
+    }
+
+    public class WordStatus
+    {
+        public string Word { get; set; }
+        public string PlayerId { get; set; }
     }
 }
--- SpaceDuck.UserService/SpaceDuck.Common/Models/Room.cs
+++ SpaceDuck.UserService/SpaceDuck.Common/Models/Room.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
 
 namespace SpaceDuck.Common.Models
 {
@@ -8,8 +11,49 @@ namespace SpaceDuck.Common.Models
         [Key]
         public int Id { get; set; }
         public GameType GameType { get; set; }
-        public RoomConfiguration RoomConfiguration { get; set; }
-        public List<string> PlayersIds { get; set; }
+        public int RoomConfigurationid { get; set; }
+        [ForeignKey("RoomConfigurationid")]
+        public virtual RoomConfiguration RoomConfiguration { get; set; }
+        [NotMapped]
+        public List<Player> Players { get; set; }
+        public string PlayerIdsFlat
+        {
+            get {
+                StringBuilder sb = new StringBuilder();
+
+                foreach (var item in Players)
+                {
+                    sb.Append($"{item.Id}:{item.Name},");
+                }
+                sb.Remove(sb.Length - 1, 1);
+
+                return sb.ToString();
+            }
+            set {
+                var players = value.Split(',').ToList();
+
+                Players = new List<Player>();
+                foreach (var item in players)
+                {
+                    var values = item.Split(':').ToArray();
+                    var player = new Player();
+                    player.Id = values[0];
+                    if (values.Length > 1)
+                        player.Name = values[1];
+                    else
+                        player.Name = "";
+                    Players.Add(player);
+                }
+            }
+        }
         public bool IsFull { get; set; }
+    }
+
+    public class Player
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+
     }
 }
