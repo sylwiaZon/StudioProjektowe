725e413b31cf6232c00ca172ad7785edb58022b3
ania wojcik <aniii.wojcik@gmail.com>
2020-05-20 14:54:36 +0200

    Update

---
+++ SpaceDuck.UserService/SpaceDuck.UserService/Services/UserService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SpaceDuck.UserService.Models;
+using System.Threading.Tasks;
+
+namespace SpaceDuck.UserService.Services
+{
+    public interface IUserService
+    {
+        Task<ApplicationUser> GetUserById(string id);
+    }
+    public class UserService : IUserService
+    {
+        private UserManager<User> userManager;
+
+        public UserService(UserManager<User> userMgr)
+        {
+            userManager = userMgr;
+        }
+
+        public async Task<ApplicationUser> GetUserById(string id)
+        {
+            var user = await userManager.FindByIdAsync(id);
+
+            return ApplicationUser.MapFromUser(user);
+        }
+    }
+}
