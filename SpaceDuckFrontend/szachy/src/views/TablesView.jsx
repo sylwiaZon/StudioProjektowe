e8e847331577d8698e75cb75372fa0d43cca8019
Karolina Pieszczek <34057260+pyszczekk@users.noreply.github.com>
2020-05-22 19:34:20 +0200

    handle private table

--- SpaceDuckFrontend/szachy/src/views/TablesView.jsx
+++ SpaceDuckFrontend/szachy/src/views/TablesView.jsx
@@ -7,6 +7,7 @@ import address from '../configuration.json';
 import history from '../history.jsx';
 import Cookies from 'universal-cookie';
 import ErrorInfo from '../components/ErrorInfo.jsx';
+import './passwordPopupStyle.css'
 const cookies = new Cookies();
 class Tables extends React.Component {
     constructor() {
@@ -14,7 +15,17 @@ class Tables extends React.Component {
         this.state = {
             tables: [],
             errorInfo:false,
+            showPasswordPopup:false,
+            password:'',
+            correctPassword:'',
+            table:''
+
         };
+        this.handlePassword = this.handlePassword.bind(this);
+        this.showPopup = this.showPopup.bind(this);
+        this.passwordPopup = this.passwordPopup.bind(this);
+        this.handleClosePopup = this.handleClosePopup.bind(this);
+        this.addToPrivateTable=this.addToPrivateTable.bind(this);
     }
 
     componentDidMount(){
@@ -30,13 +41,72 @@ class Tables extends React.Component {
             });
     }
 
+    async addToGame(){
+        var user = cookies.get('user');
+        try{
+            const response = await fetch('https://'+address.szachyURL+address.room+'/'+this.state.table.id+'/'+user.id+'/'+user.userName, {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json' 
+                },
+                body: JSON.stringify({
+                    "RoomId": this.state.table.id,
+                    "PlayerId": user.id,
+                    "PlayerName":user.userName
+                }),
+            });
+
+            if(!response.ok){
+                throw Error(response.statusText);
+            }
+
+            const json = await response.json();
+            console.log(json);
+        } catch(error){
+            this.setState({errorInfo: true});
+        }
+    }
+
+    showPopup(arg){
+        this.setState({showPasswordPopup:true, correctPassword:arg.roomConfiguration.password, table:arg})
+
+        console.log('i am the popup')
+        console.log('i am password:'+arg.roomConfiguration.password);
+       
+    }
+        handlePassword(event){
+        this.setState({password: event.target.value})
+    }
+    handleClosePopup(){
+        this.setState({showPasswordPopup:false})
+    }
+    async addToPrivateTable(){
+        cookies.set('currentTable', this.state.table, { path: '/' });
+        await this.addToGame().then(()=>{history.push('/game');});
+    }
+    passwordPopup(){
+        return(
+            <div className="overlay">
+                <div className="instructionPopup passwordPopup">  
+                    <a className="close" href='#' onClick={this.handleClosePopup}>X</a>
+                    <h2>Podaj hasło do stołu</h2>
+                    <input type="text" value={this.state.password} onChange={this.handlePassword}/>
+                    {(this.state.password==this.state.correctPassword) ? <button onClick={this.addToPrivateTable}>graj</button> : <p>zle haslo</p>}
+                </div>
+            </div>
+
+            )
+    }
+
     render() {
-      
+       
         return (
             <div className="app">
             {this.state.errorInfo ? <ErrorInfo {...{
                 visible: ()=>{this.setState({errorInfo:false})}
             }}/> : null}
+            {this.state.showPasswordPopup ? this.passwordPopup() : null}
                 <Header/>
                 <div className="szachy-header"><p>Szachy</p></div>
                 <div className="tables">
@@ -50,7 +120,7 @@ class Tables extends React.Component {
                     <ul>
                         {this.state.tables.map(arg => 
                             <li key={arg.id}>
-                                <Table id={arg.id} players={arg.players} isFull={arg.isFull} roomConfiguration={arg.roomConfiguration} table={arg}/>
+                                <Table id={arg.id} players={arg.players} isFull={arg.isFull} roomConfiguration={arg.roomConfiguration} table={arg} passwordPopup={()=>{this.showPopup(arg)}}/>
                             </li>
                         )}
                     </ul>
