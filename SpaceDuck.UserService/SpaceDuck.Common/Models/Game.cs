87e825a35025a0232e04e1b9675241e7bc14d880
sylwiaZon <sylwia.zon@gmail.com>
2020-05-22 19:37:36 +0200

    Add ships service

--- SpaceDuck.UserService/SpaceDuck.Common/Models/Game.cs
+++ SpaceDuck.UserService/SpaceDuck.Common/Models/Game.cs
@@ -15,6 +15,11 @@ namespace SpaceDuck.Common.Models
         public Queue<string> SubmittedForDrawingQue { get; set; } = new Queue<string>();
     }
 
+    public class ShipsGame : Game
+    {
+        public List<string> Players { get; set; }
+    }
+
     public class GameStatus
     {
         public string Word { get; set; } = "";
@@ -31,5 +36,32 @@ namespace SpaceDuck.Common.Models
         public string Word { get; set; }
         public string PlayerId { get; set; }
         public string PlayerName { get; set; }
+    }
+
+    public class ShipsField
+    {
+        public bool IsShip { get; set; } = false;
+        public bool IsShot { get; set; } = false;
+        public bool IsSunk { get; set; }
+        public ShipType shipType { get; set; }
+        public int IntCoordinates { get; set; }
+        public char CharCoordinates { get; set; }
+    }
+
+    public class ShipsBoard
+    {
+        public string PlayerId { get; set; }
+        public string PlayerName { get; set; }
+        public ShipsField[][] Board { get; set; }
+    }
+    
+    public class ShipsGameStatus
+    {
+        public ShipsBoard[] Boards { get; set; }
+        public string CurrentPlayerId { get; set; }
+        public string CurrentPlayerName { get; set; }
+        public int RoundTime { get; set; } = 0;
+        public bool IsFinished { get; set; }
+        public ShipsField CurrentMove { get; set; }
     }
 }
---
+++ SpaceDuck.UserService/SpaceDuck.StatkiGame/Migrations/ApplicationData/20200422102351_AddDataRelation2.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SpaceDuck.ShipsGame.Migrations.ApplicationData
+{
+    public partial class AddDataRelation2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rooms_RoomConfigurations_RoomConfigurationid",
+                table: "Rooms");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_RoomConfigurations",
+                table: "RoomConfigurations");
+
+            migrationBuilder.RenameTable(
+                name: "RoomConfigurations",
+                newName: "RoomConfiguration");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_RoomConfiguration",
+                table: "RoomConfiguration",
+                column: "RoomConfigurationid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rooms_RoomConfiguration_RoomConfigurationid",
+                table: "Rooms",
+                column: "RoomConfigurationid",
+                principalTable: "RoomConfiguration",
+                principalColumn: "RoomConfigurationid",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rooms_RoomConfiguration_RoomConfigurationid",
+                table: "Rooms");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_RoomConfiguration",
+                table: "RoomConfiguration");
+
+            migrationBuilder.RenameTable(
+                name: "RoomConfiguration",
+                newName: "RoomConfigurations");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_RoomConfigurations",
+                table: "RoomConfigurations",
+                column: "RoomConfigurationid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rooms_RoomConfigurations_RoomConfigurationid",
+                table: "Rooms",
+                column: "RoomConfigurationid",
+                principalTable: "RoomConfigurations",
+                principalColumn: "RoomConfigurationid",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
