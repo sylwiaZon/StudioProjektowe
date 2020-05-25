cddc39a167d04f5f53327516aac4501b99e85b2a
Damian Kozyra <fizomik@gmail.com>
2020-05-25 16:57:49 +0200

    chinczyk

---
+++ SpaceDuckFrontend/chinczyk/src/RouterApp.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Tables from './views/TablesView.jsx';
+import Chinczyk from './views/Chinczyk.jsx';
+import Game from './views/GameView.jsx';
+import Pawn from './views/PawnChooseView.jsx';
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={Chinczyk} />
+      <Route path="/tables" component={Tables} />
+      <Route path="/game" component={Game} />
+      <Route path="/pawn" component={Pawn} />
+
+       <Route component={Chinczyk} />
+      />
+    </Switch>
+  );
+}
