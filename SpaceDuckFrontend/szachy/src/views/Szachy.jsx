158726e752c00b8b96d9cd9c13f5be2746d51bf9
Karolina Pieszczek <34057260+pyszczekk@users.noreply.github.com>
2020-05-27 09:54:44 +0200

    ranking - players name

--- SpaceDuckFrontend/szachy/src/views/Szachy.jsx
+++ SpaceDuckFrontend/szachy/src/views/Szachy.jsx
@@ -25,7 +25,26 @@ class Szachy extends React.Component {
        this.goToMainService = this.goToMainService.bind(this);
        this.saveGuestName  = this.saveGuestName.bind(this);      
     }
+    async getUserInfo(userId) {
+        try{
+            const response = await fetch('https://localhost:5000/api/user/info/'+userId, {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json' 
+                }
+            });
 
+            if(!response.ok){
+                throw Error(response.statusText);
+            }
+
+            var data = await response.json();
+        } catch(error){
+            this.setState({errorInfo: true})
+        }
+        return data;
+    }
     componentDidMount(){
         fetch('https://'+address.szachyURL+address.ranking+"/top/5", {
                 method: 'GET',
@@ -37,13 +56,31 @@ class Szachy extends React.Component {
             .then(response => response.json())
             .then(data => {
                 var it = 1;
-                data.map((arg) => {
-                    arg.place = it;
-                    it = it + 1;
-                    this.state.ranking.push(arg);
+                Promise.all(
+                    data.map((arg) => {
+                        arg.place = it;
+                        it = it + 1;
+                        this.state.ranking.push(arg);
+
+                        return fetch('https://localhost:5000/api/user/info/'+arg.userId, {
+                            method: 'GET',
+                            headers: {
+                                'Accept': 'application/json',
+                                'Content-Type': 'application/json' 
+                            }
+                        });
+                }))
+                .then(response => Promise.all(response.map(r => r.json())))
+                .then(data => {
+                    this.state.ranking.forEach(r => r.name = data.find(u => u.id == r.userId).userName);
+                })
+                .then(()=>{
+                    this.setState({rankingLoaded: true});
+                })
+                .catch((error) => {
+                    this.setState({errorInfo:true})
                 });
             })
-            .then(()=>this.setState({rankingLoaded: true}))
             .catch((error) => {
                 this.setState({errorInfo:true})
             });
@@ -51,7 +88,7 @@ class Szachy extends React.Component {
 
     getRanking(){
         return  this.state.ranking.map(arg => 
-            <h3>{arg.place}. {arg.points}</h3>
+            <h3>{arg.place}. {arg.name}</h3>
         )
     }
     playAsGuest(){
